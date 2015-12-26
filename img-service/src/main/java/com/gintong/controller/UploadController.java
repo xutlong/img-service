851729e36d56c4a386ee123e2cf99c131ecf1d4d
xutianlong <xutianlong@gintong.com>
2015-12-26 17:20:26 +0800

    ccvvccvcv
    Signed-off-by: xutianlong <xutianlong@gintong.com>

--- img-service/src/main/java/com/gintong/controller/UploadController.java
+++ img-service/src/main/java/com/gintong/controller/UploadController.java
@@ -25,7 +25,7 @@ public class UploadController {
 	       InputStream is = jarFile.getInputStream();
 	       File tempFile = new File(uploadFilePath + _fileName); 
 	       // 开始保存文件到服务器    的男的女的
-	       if(!_fileName.equals("")) {
+	       if(!_fileName.equals("")) {// mihao 
 	    	   FileOutputStream fos = new FileOutputStream(uploadFilePath + _fileName);
 	    	   byte[] buffer = new byte[8192]; // 每次读8K字节  
 	           int count = 0;  
