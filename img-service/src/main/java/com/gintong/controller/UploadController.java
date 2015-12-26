914515c71ef67286d7a9974de4caed4408fef288
xutlong126 <xutlong@126.com>
2015-12-26 16:44:18 +0800

    删除所有注释 Signed-off-by: xutlong126 <xutlong@126.com>

--- img-service/src/main/java/com/gintong/controller/UploadController.java
+++ img-service/src/main/java/com/gintong/controller/UploadController.java
@@ -24,20 +24,16 @@ public class UploadController {
 	       String _fileName = jarFile.getOriginalFilename();
 	       InputStream is = jarFile.getInputStream();
 	       File tempFile = new File(uploadFilePath + _fileName); 
-	       // 开始保存文件到服务器
 	       if(!_fileName.equals("")) {
 	    	   FileOutputStream fos = new FileOutputStream(uploadFilePath + _fileName);
-	    	   byte[] buffer = new byte[8192]; // 每次读8K字节  
+	    	   byte[] buffer = new byte[8192];
 	           int count = 0;  
-	           // 开始读取上传文件的字节，并将其输出到服务端的上传文件输出流中  
 	           while ((count = is.read(buffer)) > 0) {  
-	               fos.write(buffer, 0, count); // 向服务端文件写入字节流  
+	               fos.write(buffer, 0, count);   
 	           } 
-	           // 关闭FileOutputStream对象  
 	           fos.close(); 
-	           // InputStream对象 
 	           is.close(); 
-	       }// 你好哦 456
+	       }
 	    } catch (UnsupportedEncodingException e) {  
 	        e.printStackTrace();  
 	        return "fild";
