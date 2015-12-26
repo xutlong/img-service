e0950013853acc27b4104be573f63d684e5d74ce
xutianlong <xutianlong@gintong.com>
2015-12-26 17:04:01 +0800

    Revert "vvvv     的地方地方大幅度发 Signed-off-by: xutianlong <xutianlong@gintong.com>"
    This reverts commit 9b2483f541f2cf3e7b48971a93bac2cff2d6d74d.

--- img-service/src/main/java/com/gintong/controller/UploadController.java
+++ img-service/src/main/java/com/gintong/controller/UploadController.java
@@ -24,14 +24,18 @@ public class UploadController {
 	       String _fileName = jarFile.getOriginalFilename();
 	       InputStream is = jarFile.getInputStream();
 	       File tempFile = new File(uploadFilePath + _fileName); 
+	       // 开始保存文件到服务器
 	       if(!_fileName.equals("")) {
 	    	   FileOutputStream fos = new FileOutputStream(uploadFilePath + _fileName);
-	    	   byte[] buffer = new byte[8192];
+	    	   byte[] buffer = new byte[8192]; // 每次读8K字节  
 	           int count = 0;  
+	           // 开始读取上传文件的字节，并将其输出到服务端的上传文件输出流中  
 	           while ((count = is.read(buffer)) > 0) {  
-	               fos.write(buffer, 0, count);   
+	               fos.write(buffer, 0, count); // 向服务端文件写入字节流  
 	           } 
+	           // 关闭FileOutputStream对象  
 	           fos.close(); 
+	           // InputStream对象 
 	           is.close(); 
 	       }
 	    } catch (UnsupportedEncodingException e) {  
@@ -41,6 +45,7 @@ public class UploadController {
 	        e.printStackTrace();  
 	        return "fild";
 	    }  
+	    // TODO �����ļ�����...  
 	    return "OK"; 
 	}
 }
