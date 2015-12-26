89223af702ea2fcafde43373fd64e1af006fb152
xutlong126 <xutlong@126.com>
2015-12-26 16:39:47 +0800

    chongji jiejue Signed-off-by: xutlong126 <xutlong@126.com>

--- img-service/src/main/java/com/gintong/controller/UploadController.java
+++ img-service/src/main/java/com/gintong/controller/UploadController.java
@@ -37,7 +37,7 @@ public class UploadController {
 	           fos.close(); 
 	           // InputStream对象 
 	           is.close(); 
-	       }// 你好哦
+	       }// 你好哦 456
 	    } catch (UnsupportedEncodingException e) {  
 	        e.printStackTrace();  
 	        return "fild";
